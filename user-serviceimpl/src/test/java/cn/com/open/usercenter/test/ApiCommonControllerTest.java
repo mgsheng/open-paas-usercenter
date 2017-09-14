d478e71770ca288373aca6f157691617129926ca
guxuyang <it_gxy@163.com>
2017-09-14 14:10:27 +0800

    单元测试

---
+++ user-serviceimpl/src/test/java/cn/com/open/usercenter/test/ApiCommonControllerTest.java
@@ -0,0 +1,54 @@
+package cn.com.open.usercenter.test;
+
+import cn.com.open.openpaas.userservice.app.tools.DES;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.Filter;
+import java.util.UUID;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath*:/spring/appCtx-disconf.xml",
+        "classpath*:/spring/context.xml", "classpath*:/spring/job.xml",
+        "classpath*:/spring/security.xml", "classpath*:/spring/transaction.xml" })
+public class ApiCommonControllerTest {
+
+    private MockHttpServletRequest request;
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        request = new MockHttpServletRequest();
+        request.setCharacterEncoding("UTF-8");
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).addFilter(springSecurityFilterChain).build();
+    }
+
+    @Test
+    public void commonStatusTest() {
+        try {
+            MvcResult result = mockMvc.perform(
+                    MockMvcRequestBuilders.get("/common/status")).andReturn();
+            System.out.println(result.getResponse().getContentAsString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
